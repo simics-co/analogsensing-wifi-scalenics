77e42a4c951a47288ccd8aaa6beeeb8f452dd222
loonaifactory <loonaifactory@gmail.com>
2016-07-13 11:33:29 +0900

    Update source code and README

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# analogsensing-wifi-scalenics
-Sense analog and upload the value to Scalenics with Wi-Fi
+Analog Sensing for ESPr One(ESP-WROOM-02)
+====
+
+「Analog Sensing for ESPr One(ESP-WROOM-02)」は、[ESPr One(ESP-WROOM-02 Arduino互換ボード)](https://www.switch-science.com/catalog/2620/)を使って、
+アナログセンシング値をIoTクラウド：[Scalenics](http://scalenics.io/)へグラフ表示することができます。
+
+## Description
+本スケッチは、ESPr OneのA0に入力されたアナログ値を5秒間隔でセンシングし、
+IoTクラウド：[Scalenics](http://scalenics.io/)へその値をアップロードします。
+
+＜注:ESPr Oneのスペック＞
+* アナログ入力 : A0の1つのみです。入力範囲は0～3.3 V（基板上で0～1.0 Vに変換しています）
+* 10 bitA/Dコンバータを搭載しています
+
+## Demo
+
+センシングした値を、[Scalenics](http://scalenics.io/)へグラフ表示します（以下のグラフは、温度に変換した例です）
+
+![TemperatureDemo](images/TemperatureDemo.png "TemperatureDemo")<br>
+
+## Requirement
+
+* [ESPr One(ESP-WROOM-02 Arduino互換ボード)](https://www.switch-science.com/catalog/2620/)を用意してください
+* [Arduino LLC (Arduino.cc)](https://www.arduino.cc/) Arduino IDE 1.6.4以降のバージョンを使用してください<br>
+  ※[Arduino SRL (Arduino.org)](http://www.arduino.org/)系のArduino IDE 1.7.Xでは、ESP8266ボードを追加できません
+* [Scalenics](http://scalenics.io/)のアカウントが必要です
+
+## Usage
+
+1. 「analogsensing-wifi-scalenics.ino」を実行します
+2. スケッチ内の以下の項目を変更します<br>
+　![ChangeItems](images/ChangeItems.png "ChangeItems")
+   * ① 接続するルーターのIPアドレスが固定の場合は、そのIPアドレス, デフォルトゲートウェイ, サブネットマスクに書き換えます（通常、変更する必要はありません）
+   * ② 接続する無線ルーターのSSIDに書き換えます
+   * ③ 接続する無線ルーターの暗号化キー（パスワード）に書き換えます
+   * ④ ScalenicsのUsername(ID)に書き換えます
+   * ⑤ ScalenicsのManagement Console -> Admin Profile内にある「Device Token」に書き換えます
+   * ⑥ 任意のセンサーIDに書き換えます
+
+## Install
+
+1. [このサイト](http://trac.switch-science.com/wiki/esp_dev_arduino_ide)を参考に、Arduino IDEへESP8266ボードを追加します<br>
+   ※Arduino IDE 1.6.9の設定例は、以下の通りです<br>
+   ![ChangeItems](images/Settings.png "Settings")
+2. [Arduino Client for MQTT](https://github.com/knolleary/pubsubclient)をインストールします<br>
+   ※インストール手順は、「[EnOcean Library for Arduino](https://github.com/simics-co/EnOcean)」のREADME.md：Install 1～4を参考にしてください<br>
+3. [GitHub repository](https://github.com/simics-co/analogsensing-wifi-scalenics)から[analogsensing-wifi-scalenics-master.zip](https://github.com/simics-co/analogsensing-wifi-scalenics/archive/master.zip)をダウンロードします
+4. 圧縮ファイルを展開し、フォルダ名を「analogsensing-wifi-scalenics.ino」へ変更します
+
+## Contribution
+
+1. Forkする ( https://github.com/simics-co/EnOcean#fork-destination-box )
+2. ローカル上でbranchを作る (git checkout -b my-new-feature)
+3. 変更した内容をcommitする (git commit -am 'Add some feature')
+4. pushする (git push origin my-new-feature)
+5. Pull Requestを作成する
+
+## Licence
+
+   Copyright 2016 LoonaiFactory
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+## Author
+
+[LoonaiFactory](https://github.com/loonaifactory)
---
+++ analogsensing-wifi-scalenics.ino
@@ -0,0 +1,147 @@
+/*
+  Analog Sensing for ESPr One(ESP-WROOM-02)
+
+  This sketch connects to a IoT cloud "Scalenics" (https://api.scalenics.io/console/)
+  using ESP-WROOM-02 (Wi-Fi).
+
+  The circuit:
+  *Input PIN
+    A0:Analog in
+  *Output PIN
+    None
+
+  Created 13 July 2016
+  by LoonaiFactory
+
+  https://github.com/simics-co/analogsensing-wifi-scalenics
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Connection info data lengths
+#define MAC_ADDR_LEN    6   // Length of MAC address in bytes
+
+// Constants
+unsigned char mac_addr[MAC_ADDR_LEN];
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 1, 0);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+// WiFi
+const char* ssid     = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Scalenics
+#define SC_USER "YOUR_SCALENICS_ACCOUNT"
+#define DEVICE_TOKEN "YOUR_DEVICE_TOKEN_HERE"
+#define CLIENT_ID "enocean"
+#define MQTT_SERVER "api.scalenics.io"
+
+const uint32_t deviceID = 0x01234567;
+
+WiFiClient client;
+
+// MQTT client
+PubSubClient mqttClient(MQTT_SERVER, 1883, NULL, client);
+
+static void connect(void);
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  connect();
+}
+
+#define TIME_DELAY 5000  /* msec */
+String topic;
+String postData;  
+char mqtt_topic[64];
+char mqtt_payload[32];
+
+const int sensorPin = A0;
+char postID[4];
+char state[5];
+
+void loop()
+{
+  delay(TIME_DELAY);
+  
+  int sensorValue = analogRead(sensorPin);
+  
+  topic = DEVICE_TOKEN;
+  topic += "/";
+  sprintf(postID, "%08lX", deviceID);
+  topic += postID;
+  topic.toCharArray(mqtt_topic, topic.length() + 1);
+  
+  postData = "";
+  postData += "v=";
+  sprintf(state, "%d", sensorValue);
+  postData += state;
+  postData.toCharArray(mqtt_payload, postData.length() + 1);
+  
+  Serial.print(F("  mqtt_topic : "));
+  Serial.println(mqtt_topic);
+  Serial.print(F("  mqtt_payload : "));
+  Serial.println(mqtt_payload);
+  
+  if (mqttClient.connect(postID, SC_USER, DEVICE_TOKEN)) {
+    Serial.println(F("  Connect to MQTT server.."));
+    mqttClient.publish(mqtt_topic, mqtt_payload);
+    Serial.println(F("  Disconnecting MQTT server.."));
+    mqttClient.disconnect();
+  } else {
+    Serial.println(F("  Connection to MQTT server failed"));
+  }
+}
+
+static void connect(void)
+{
+  Serial.println();
+  
+  // give the wifi module time to boot up:
+  delay(100);
+  
+  // read and display MAC address
+  String mac = WiFi.macAddress();
+  Serial.print(F("MAC Address: "));
+  Serial.println(mac);
+  
+  // start the Ethernet connection:
+  Serial.print(F("Connecting to "));
+  Serial.print(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  uint8_t timeout = 60; // 60 * 500 msec = 30 sec time out
+  while ((WiFi.status() != WL_CONNECTED) && timeout) {
+    delay(500);
+    Serial.print(F("."));
+    timeout--;
+  }
+  Serial.println();
+  
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.print(F("Setting static IP"));
+    WiFi.config(ip, gateway, subnet);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(F("."));
+    }
+    Serial.println();
+  }
+  
+  Serial.println(F("WiFi connected"));
+  Serial.print(F("IP address: "));
+  Serial.println(WiFi.localIP());
+}
